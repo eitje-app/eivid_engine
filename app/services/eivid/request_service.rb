c7462134ab4129cdec3752948c3c77c2d5d397fe
Jurriaan Schrofer <jschrofer@gmail.com>
2021-03-29 16:18:35 +0200

    pass body.json in request while putting back "Content-Type" => "application/json" in order to being able to pass "tus" as header strategy

--- app/services/eivid/request_service.rb
+++ app/services/eivid/request_service.rb
@@ -21,23 +21,44 @@ module Eivid::RequestService
     end
 
     def upload_video(video_path:)
-      file = File.open(video_path) 
+      @file = File.open(video_path) 
 
       body = {
         "upload" => {
           "approach" => "tus",
-          "size"     => file.size.to_s,
+          "size"     => @file.size.to_s,
         }
       }
 
       headers = {
         "Authorization" => "bearer #{Figaro.env.VIMEO_ACCESS_TOKEN}",
+        "Content-Type"  => "application/json",
         "Accept"        => "application/vnd.vimeo.*+json;version=3.4" 
       }
 
-      options = { body: body, headers: headers }
+      options = { body: body.to_json, headers: headers }
 
-      res = HTTParty.post UPLOAD_URL, **options
+      @request  = HTTParty.post UPLOAD_URL, **options
+      @response = JSON.parse(@request).deep_symbolize_keys
+
+      @upload_link = @response.dig(:upload, :upload_link)
+      @video_link  = @response.dig(:link)
+      @video_uri   = "#{BASE_URL}/#{@response.dig(:uri)}"
+      @approach    = @response.dig(:upload, :approach)
+
+      # binding.pry
+      upload_to_vimeo
+    end
+
+    def upload_to_vimeo
+      headers = {
+        "Tus-Resumable" => "1.0.0",
+        "Upload-Offset" => "0",
+        "Content-Type"  => "application/offset+octet-stream",
+        "Accept"        => "application/vnd.vimeo.*+json;version=3.4"
+      }
+
+      # res = HTTParty.patch @upload_link, body: @file.read, headers: headers
       binding.pry
     end
 
