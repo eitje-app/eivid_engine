988d033e910bb911a7cbf38657e3f2ca325d175e
Jurriaan Schrofer <jschrofer@gmail.com>
2021-03-30 12:32:13 +0200

    get a folder_id on effect before_create for Eivid::Owner through Vimeo API request

--- app/models/eivid/concerns/request_service/manage_folder.rb
+++ app/models/eivid/concerns/request_service/manage_folder.rb
@@ -9,17 +9,21 @@ module Eivid::Concerns::RequestService::ManageFolder
     HTTParty.get Eivid::RequestService::FOLDER_URL, headers: folder_headers
   end
 
-  def create_folder(namespace: "org", id:)
-    body = {
-      "name" => "#{namespace}_#{org_id}"
-    }
-    HTTParty.post Eivid::RequestService::FOLDER_URL, body: body, headers: folder_headers
+  def create_folder(namespace:, id:)
+    body = { "name" => "#{namespace}_#{id}" }
+    
+    @response = HTTParty.post Eivid::RequestService::FOLDER_URL, body: body.to_json, headers: folder_headers
+    get_folder_id
   end
 
   private
 
+  def get_folder_id
+    @response["uri"].split('/').last
+  end
+
   def folder_headers
-    headers = {
+    {
       "Authorization" => "bearer #{Figaro.env.VIMEO_ACCESS_TOKEN}",
       "Content-Type"  => "application/json",
       "Accept"        => "application/vnd.vimeo.*+json;version=3.4" 
--- app/models/eivid/owner.rb
+++ app/models/eivid/owner.rb
@@ -3,10 +3,18 @@ module Eivid
 
     has_many :videos
 
+    validates :external_id, presence: true, uniqueness: true
+
+    before_create :get_folder_id
+
     belongs_to :"#{Eivid.owner_model}", foreign_key: "external_id"
 
     def external_owner
       send Eivid.owner_model
+    end
+
+    def get_folder_id
+      self.folder_id = RequestService.create_folder(namespace: Eivid.owner_model, id: self.external_id)
     end
 
   end
