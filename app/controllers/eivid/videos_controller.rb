95b4ad08c52299999f671d3190aea4b573259e1e
Jurriaan Schrofer <jschrofer@gmail.com>
2021-03-30 15:30:33 +0200

    endpoint for getting all videos from an owner

--- app/controllers/eivid/videos_controller.rb
+++ app/controllers/eivid/videos_controller.rb
@@ -9,9 +9,14 @@ module Eivid
       # rake environment resque:scheduler
 
     def upload_video
-      owner  = find_owner(params["external_owner_id"])      
+      owner  = find_owner params["external_owner_id"]     
       record = UploadService.upload(owner: owner, video_file: params["video_file"])   
       render json: record
+    end
+
+    def owner_videos
+      owner = find_owner params["external_owner_id"]   
+      render json: owner.videos
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Eivid::Engine.routes.draw do
 
   resources :videos do
     post 'upload_video', on: :collection
+    get  'owner_videos', on: :collection
   end
   
 end
