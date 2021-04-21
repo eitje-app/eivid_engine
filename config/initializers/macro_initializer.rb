e6f5123f6cba1d53780848f4db72194be107fa62
Jurriaan Schrofer <jschrofer@gmail.com>
2021-04-21 15:54:26 +0200

    amazing fix for monkey patching the main app models, persistent across sessions and reload!'s

--- config/initializers/macro_initializer.rb
+++ config/initializers/macro_initializer.rb
@@ -1,11 +1,22 @@
-ApplicationRecord.define_singleton_method :eivid_owner do
-  include Eivid::Concerns::MainApp::Owner
+module Eivid::Macros
+  extend ActiveSupport::Concern
+  included do
+    class << self
+
+      def eivid_owner
+        include Eivid::Concerns::MainApp::Owner
+      end
+
+      def eivid_video_resource
+        include Eivid::Concerns::MainApp::VideoResource
+      end
+
+      def eivid_user
+        include Eivid::Concerns::MainApp::User
+      end
+
+    end
+  end
 end
 
-ApplicationRecord.define_singleton_method :eivid_video_resource do
-  include Eivid::Concerns::MainApp::VideoResource
-end
-
-ApplicationRecord.define_singleton_method :eivid_user do
-  include Eivid::Concerns::MainApp::User
-end
+::ActiveRecord::Base.send :include, Eivid::Macros
