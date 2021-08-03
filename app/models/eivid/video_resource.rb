75ad0848484821870b4842ebe87eab4ee19d6c22
Jurriaan Schrofer <jschrofer@gmail.com>
2021-08-03 15:12:22 +0200

    add touch resource for video_resource

--- app/models/eivid/video_resource.rb
+++ app/models/eivid/video_resource.rb
@@ -2,7 +2,7 @@ module Eivid
   class VideoResource < ApplicationRecord
 
     belongs_to :video
-    belongs_to :resource, polymorphic: true
+    belongs_to :resource, polymorphic: true, touch: true
 
   end
 end
