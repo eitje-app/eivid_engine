b2a3756e1c95be7be0d4a889e702e892bb5fc090
Jurriaan Schrofer <jschrofer@gmail.com>
2021-08-04 15:39:01 +0200

    add has_many touch

--- app/models/eivid/video.rb
+++ app/models/eivid/video.rb
@@ -5,6 +5,8 @@ module Eivid
     belongs_to :user, optional: true
     has_many   :video_resources
 
+    after_commit :touch_video_resources
+
     def owner_id # required for aliassing :owner_id
       super
     end
@@ -16,6 +18,11 @@ module Eivid
     alias_method :"#{Eivid.owner_model}_id", :owner_id
     alias_method :"#{Eivid.owner_model}", :external_owner
     scope        :"of_#{Eivid.owner_model}", -> (owner_id) { where(owner_id: owner_id) }
+
+    def touch_video_resources
+      # has_many touch implementation
+      video_resources.find_each { |record| record.update(updated_at: Time.now) }
+    end
 
   end
 end
