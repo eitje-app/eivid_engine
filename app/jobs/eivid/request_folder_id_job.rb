6227f84ba0843aef7a32e916714f74a1aaf8ce38
Jurriaan Schrofer <jschrofer@gmail.com>
2021-08-24 15:02:34 +0200

    request the folder id in the back

---
+++ app/jobs/eivid/request_folder_id_job.rb
@@ -0,0 +1,16 @@
+module Eivid
+  class RequestFolderIdJob < ApplicationJob
+
+    def perform(eivid_owner_id)
+
+      owner     = Owner.find(eivid_owner_id) 
+      folder_id = RequestService.create_folder(namespace: Eivid.owner_model, id: owner.id)
+
+      # binding.pry
+
+      owner.update!(folder_id: folder_id)
+
+    end
+
+  end
+end
--- app/models/eivid/owner.rb
+++ app/models/eivid/owner.rb
@@ -5,16 +5,16 @@ module Eivid
 
     validates :external_id, presence: true, uniqueness: true
 
-    before_create :get_folder_id
+    after_create :get_folder_id
 
-    belongs_to :"#{Eivid.owner_model}", foreign_key: "external_id"
+    belongs_to :"#{Eivid.owner_model}", foreign_key: "external_id", optional: true
 
     def external_owner
       send Eivid.owner_model
     end
 
     def get_folder_id
-      self.folder_id = RequestService.create_folder(namespace: Eivid.owner_model, id: self.external_id)
+      RequestFolderIdJob.perform_later(self.id)
     end
 
   end
