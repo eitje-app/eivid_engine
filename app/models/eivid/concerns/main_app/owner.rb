686c93fba6c3a802e14fd9c3009a7b76307d7822
Jurriaan Schrofer <jschrofer@gmail.com>
2021-09-25 17:06:03 +0200

    V 1.1.2

--- app/models/eivid/concerns/main_app/owner.rb
+++ app/models/eivid/concerns/main_app/owner.rb
@@ -3,7 +3,7 @@ module Eivid::Concerns::MainApp::Owner
   included do
   
     has_one  :video_owner, class_name: 'Eivid::Owner', foreign_key: 'external_id'
-    has_many :videos, class_name: 'Eivid::Video', source: :video_owner, foreign_key: 'owner_id'
+    has_many :videos, class_name: 'Eivid::Video', foreign_key: 'owner_id'
 
     include Eivid::Concerns::MainApp::VideoConcerns
 
