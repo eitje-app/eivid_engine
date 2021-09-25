c7a56d659888ed9c83ff0045d79d73daf2f85aeb
Jurriaan Schrofer <jschrofer@gmail.com>
2021-09-25 16:55:34 +0200

    V 1.1.1

--- lib/eivid/engine.rb
+++ lib/eivid/engine.rb
@@ -7,27 +7,23 @@ module Eivid
 
   end
 
-  class << self
+  mattr_accessor :owner_model, :infer_external_owner_id, :notify_front_enabled, :notify_method_on_upload,
+                 :notify_method_on_video_available, :notify_method_on_versions_available
 
-    mattr_accessor :owner_model, :infer_external_owner_id, :notify_front_enabled, :notify_method_on_upload,
-                   :notify_method_on_video_available, :notify_method_on_versions_available
+  #sets video owner in the main app
+  self.owner_model = nil
+  
+  # sets how to extract the owner_id in controller actions
+  self.infer_external_owner_id = "params['external_owner_id'].to_i"
 
-    #sets video owner in the main app
-    self.owner_model = nil
-    
-    # sets how to extract the owner_id in controller actions
-    self.infer_external_owner_id = "params['external_owner_id'].to_i"
+  # sets front end notifiers
+  self.notify_front_enabled = false
+  self.notify_method_on_upload = nil
+  self.notify_method_on_video_available = nil
+  self.notify_method_on_versions_available = nil
 
-    # sets front end notifiers
-    self.notify_front_enabled = false
-    self.notify_method_on_upload = nil
-    self.notify_method_on_video_available = nil
-    self.notify_method_on_versions_available = nil
-
-    def set_mattr_accessors(&block)
-      yield self
-    end
-
+  def self.set_mattr_accessors(&block)
+    yield self
   end
 
 end
