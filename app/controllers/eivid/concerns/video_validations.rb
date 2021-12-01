a204995b927f35fa0f93842908b5d37e0d316630
Jurriaan Schrofer <jschrofer@gmail.com>
2021-12-01 11:09:47 +0100

    V 1.1.3

--- app/controllers/eivid/concerns/video_validations.rb
+++ app/controllers/eivid/concerns/video_validations.rb
@@ -14,27 +14,21 @@ module Eivid::Concerns::VideoValidations
 
     def validate_video_file_presence
       unless params["video_file"]
-        raise Eivid::VideoFileNotPresentError.new(
-          "you forgot to add a required 'video_file' to your request"
-        )
+        raise_controller_error "you forgot to add a required 'video_file' to your request"
       end
     end
 
     def validate_video_file_format
       mime = params["video_file"]&.original_filename&.split('.')&.last&.downcase     
       unless Eivid::VideoMimeDump::DATA.include?(mime)
-        raise Eivid::IncorrectVideoMimeTypeError.new(
-          "the 'video_file' you tried to upload, is of an invalid mime type"
-        )
+        raise_controller_error "the 'video_file' you tried to upload, is of an invalid mime type"
       end
     end
 
     def validate_video_file_size
       megabytes = params["video_file"].tempfile.size / 1.0.megabyte
       unless MAX_MB_VIDEO >= megabytes
-        raise VideoFileSizeTooBigError.new(
-          "the 'video_file' size (#{megabytes.round(2)} mb) you tried to upload exceeds the maximum file size (#{MAX_MB_VIDEO} mb)"
-        )
+        raise_controller_error "the 'video_file' size (#{megabytes.round(2)} mb) you tried to upload exceeds the maximum file size (#{MAX_MB_VIDEO} mb)"
       end
     end
   
